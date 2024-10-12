5e7390c511a42ce02e1622b219134b1ccc4171b5
PradoAllan <allanprado.it@gmail.com>
2024-10-12 12:34:05 -0300

    up: understanding diff between arrayOf and intArrayOf

--- Kotlin/understandingVariables/src/Main.kt
+++ Kotlin/understandingVariables/src/Main.kt
@@ -2,19 +2,35 @@
 fun main()
 {
     var x: Int
+    var test = 24
     var name = "Allan"
     var surName = "Prado"
-    val numbers = arrayOf(42, 24, 2000, 2024)
+    val primitiveArray = intArrayOf(42, 24, 2000, 2024)
+    val nonPrimitiveArray = arrayOf(42, 24, "2000", "aprado")
 
-    x = numbers.size
+    x = primitiveArray.size
     while (x-- > 0)
     {
-        numbers[x]++
-        numbers[x]++
-        println(numbers[x])
-        name += surName
-        surName += name + numbers[x]
-        println(name + surName + numbers[x])
+        println(primitiveArray[x].javaClass)
+        println(primitiveArray[x])
+        if (primitiveArray[x].equals(test))
+            println("The value of array[$x] is ${primitiveArray[x]} and is = to the test varible $test")
+
+        //name += surName
+        //surName += name + numbers[x]
+        //println(name + surName + numbers[x])
+    }
+
+    x = nonPrimitiveArray.size
+    while (x-- > 0)
+    {
+        println(nonPrimitiveArray[x].javaClass)
+        println(nonPrimitiveArray[x])
+    }
+    for (a in nonPrimitiveArray)
+    {
+        println(a.javaClass)
+        println(a)
     }
 
 }

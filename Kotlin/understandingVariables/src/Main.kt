5433e8dd10950c8fe9e6203c7ce9bd24ef1d3871
PradoAllan <allanprado.it@gmail.com>
2024-10-22 20:34:56 -0300

    up: main.kt with new variables that I understand now

--- Kotlin/understandingVariables/src/Main.kt
+++ Kotlin/understandingVariables/src/Main.kt
@@ -2,7 +2,14 @@
 fun main()
 {
     var x: Int
-    var test = 24
+    var test = 42
+    var test2 = test // test and test2 holds the same reference of the Int object
+
+    var lonng: Long
+    lonng = test.toLong() // This creates a new Long object with the same value as the test variable
+
+    var a = 42L
+    println(a)
     var name = "Allan"
     var surName = "Prado"
     val primitiveArray = intArrayOf(42, 24, 2000, 2024)
